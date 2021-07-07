6b6223a8a5c7eec8acbf83d7f4dbc70220d6f5c2
Earl Panganiban <earl@quadrivium.xyz>
2021-07-07 17:55:05 +0800

    Added ball0; reverts to original state after random answer

--- 05-magic_8/lib/main.dart
+++ 05-magic_8/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,12 +14,32 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballIndex = 0;
+
+  void randomize() {
+    setState(() {
+      if (ballIndex == 0) {
+        ballIndex = Random().nextInt(5) + 1;
+      } else {
+        ballIndex = 0;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+        onPressed: randomize,
+        child: Image.asset('images/ball$ballIndex.png'),
+      ),
     ));
   }
 }
