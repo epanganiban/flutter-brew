11515f1f67d0e52b769374a44063d04a28e87a05
Earl Panganiban <earl@quadrivium.xyz>
2021-07-07 16:14:34 +0800

    Removed boilerplate test; implemented basic dice increment onPressed

--- 04-dicee/lib/main.dart
+++ 04-dicee/lib/main.dart
@@ -13,9 +13,60 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDie = 1;
+  int rightDie = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  if (leftDie < 6) {
+                    setState(() {
+                      leftDie++;
+                    });
+                  } else {
+                    setState(() {
+                      leftDie = 1;
+                    });
+                  }
+                },
+                child: Image.asset('images/dice$leftDie.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  if (rightDie < 6) {
+                    setState(() {
+                      rightDie++;
+                    });
+                  } else {
+                    setState(() {
+                      rightDie = 1;
+                    });
+                  }
+                },
+                child: Image.asset('images/dice$rightDie.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
