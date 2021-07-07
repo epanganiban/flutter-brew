098eefe465a65541fd8910d08cb120600416aaa9
Earl Panganiban <earl@quadrivium.xyz>
2021-07-07 22:32:06 +0800

    Completed xylophone project

--- 06-xylophone/lib/main.dart
+++ 06-xylophone/lib/main.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
+  Expanded bar(int index, Color color) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$index.wav');
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        child: Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(child: Text(nouns.last)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              bar(1, Colors.red),
+              bar(2, Colors.orange),
+              bar(3, Colors.yellow),
+              bar(4, Colors.green),
+              bar(5, Colors.blue),
+              bar(6, Colors.indigo),
+              bar(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
