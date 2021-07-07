6c9f87e1d4c6a1844844cc85fa108364de55a38f
Earl Panganiban <earl@quadrivium.xyz>
2021-07-07 10:24:23 +0800

    Moved MaterialApp to stateless widget

--- 03-mi_card/lib/main.dart
+++ 03-mi_card/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ),);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(),
+      ),
+    );
+  }
 }
