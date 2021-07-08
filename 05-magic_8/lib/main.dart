4c376c7d7d93ce3f848f022a8b23a985ad29880b
Earl Panganiban <earl@quadrivium.xyz>
2021-07-08 11:37:59 +0800

    Added more responses to magic_8; renders text instead of image

--- 05-magic_8/lib/main.dart
+++ 05-magic_8/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
 import 'dart:math';
 
 void main() {
@@ -21,14 +22,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int ballIndex = 0;
+  int ballState = 0;
+  int responseText = 0;
+
+  var responses = [
+    const Tuple2<String, double>('', 16.0),
+    const Tuple2<String, double>('IT IS\nUNCLEAR', 16.0),
+    const Tuple2<String, double>('IT IS DECIDEDLY\nSO', 14.0),
+    const Tuple2<String, double>('WHEN PIGS FLY', 16.0),
+    const Tuple2<String, double>('YOU MAY\nRELY ON\nIT',16.0),
+    const Tuple2<String, double>('MY SOURCES SAY NO',16.0),
+    const Tuple2<String, double>('AS I SEE\nIT, YES',16.0),
+    const Tuple2<String, double>('OUTLOOK GOOD',16.0),
+    const Tuple2<String, double>('REPLY HAZY TRY AGAIN',14.0),
+    const Tuple2<String, double>('PROBABLY NOT',15.0),
+    const Tuple2<String, double>('SIGNS POINT TO YES',16.0),
+    const Tuple2<String, double>('ASK AGAIN LATER',16.0),
+    const Tuple2<String, double>('BETTER NOT TELL YOU\nNOW',14.0),
+    const Tuple2<String, double>('MOST\nLIKELY',18.0),
+    const Tuple2<String, double>('CONCENTRATE AND ASK AGAIN',11.0),
+    const Tuple2<String, double>('WITHOUT A DOUBT', 16.0),
+    const Tuple2<String, double>('DON\'T COUNT\nON IT',16.0),
+    const Tuple2<String, double>('MY REPLY\n IS NO',16.0),
+    const Tuple2<String, double>('CANNOT PREDICT\nNOW',16.0),
+    const Tuple2<String, double>('OUTLOOK NOT SO GOOD',16.0),
+    const Tuple2<String, double>('VERY DOUBTFUL',15.0),
+  ];
+
+  Text buildText(Tuple2<String, double> response) {
+    return Text(
+      response.item1,
+      style: TextStyle(
+        color: Colors.lightBlueAccent,
+        fontWeight: FontWeight.bold,
+        fontSize: response.item2,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
 
   void randomize() {
     setState(() {
-      if (ballIndex == 0) {
-        ballIndex = Random().nextInt(5) + 1;
-      } else {
-        ballIndex = 0;
+      switch (ballState) {
+        case 0:
+          {
+            ballState = 1;
+            responseText = Random().nextInt(20)+1;
+          }
+          break;
+        case 1:
+          {
+            ballState = 0;
+            responseText = 0;
+          }
+          break;
       }
     });
   }
@@ -39,7 +86,18 @@ class _MyAppState extends State<MyApp> {
         child: Center(
       child: TextButton(
         onPressed: randomize,
-        child: Image.asset('images/ball$ballIndex.png'),
+        child: Stack(
+          children: [
+            Center(child: Image.asset('images/ball$ballState.png')),
+            Align(
+              alignment: Alignment(0.02, -0.03),
+              child: Container(
+                child: buildText(responses[responseText]),
+                width: 85.0,
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
