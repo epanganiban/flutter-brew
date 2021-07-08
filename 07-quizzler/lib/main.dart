a106811079b878140fea73e3de0d9d74082f6f42
Earl Panganiban <earl@quadrivium.xyz>
2021-07-08 16:30:53 +0800

    Created question class

--- 07-quizzler/lib/main.dart
+++ 07-quizzler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,15 +29,19 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   int currentIndex = 0;
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questions = [
+    Question(
+      q: 'You can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
   ];
 
   void evaluateAnswer(bool response) {
@@ -57,7 +62,7 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   void trackScore(bool response, int index) {
-    switch (response == answers[index]) {
+    switch (response == questions[index].answer) {
       case true:
         {
           scoreKeeper.add(Icon(Icons.check, color: Colors.green));
@@ -83,7 +88,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[currentIndex],
+                questions[currentIndex].text!,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
---
+++ 07-quizzler/lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String? text;
+  bool? answer;
+
+  // Question(this.text, this.answer);
+  Question({required String q, required bool a}) {
+    this.text = q;
+    this.answer = a;
+  }
+}
