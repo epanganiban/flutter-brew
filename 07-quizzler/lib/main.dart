60de61221b1e3abd6c32e8d72c71bdd663d665d3
Earl Panganiban <earl@quadrivium.xyz>
2021-07-08 15:14:50 +0800

    Added sample set of questions

--- 07-quizzler/lib/main.dart
+++ 07-quizzler/lib/main.dart
@@ -26,7 +26,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int currentIndex = 0;
   List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[currentIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -57,7 +63,11 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  if (currentIndex < questions.length-1) {
+                    currentIndex++;
+                  } else {
+                    currentIndex = 0;
+                  }
                 });
               },
               style: TextButton.styleFrom(
@@ -80,7 +90,11 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  if (currentIndex < questions.length-1) {
+                    currentIndex++;
+                  } else {
+                    currentIndex = 0;
+                  }
                 });
               },
               style: TextButton.styleFrom(
