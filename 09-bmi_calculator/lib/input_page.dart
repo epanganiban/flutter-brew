e409cbd0c6ef97e023343701c0f34c5aaf412c26
Earl Panganiban <earl@quadrivium.xyz>
2021-07-09 16:17:20 +0800

    Moved RoundIconButton to its own file

--- 09-bmi_calculator/lib/input_page.dart
+++ 09-bmi_calculator/lib/input_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
+import 'round_icon_button.dart';
 
 enum Gender {
   female,
@@ -241,26 +244,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Function onPressed;
-  RoundIconButton({@required this.icon, @required this.onPressed});
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      onPressed: onPressed,
-      child: Icon(this.icon),
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      elevation: 0.0,
-      fillColor: Color(0xFF4C4F5E),
-      shape: CircleBorder(),
     );
   }
 }
