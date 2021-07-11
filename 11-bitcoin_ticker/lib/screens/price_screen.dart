d306b7f0b5b5a1a9caaf875cd0f9d5ba90cc3aa0
Earl Panganiban <earl@quadrivium.xyz>
2021-07-11 15:27:39 +0800

    Moved RateCard component to its own file

--- 11-bitcoin_ticker/lib/screens/price_screen.dart
+++ 11-bitcoin_ticker/lib/screens/price_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bitcoin_ticker/utils/coin_data.dart';
+import 'package:bitcoin_ticker/components/rate_card.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -112,43 +113,6 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class RateCard extends StatelessWidget {
-  RateCard({
-    required this.base,
-    required this.quote,
-    required this.rate,
-  });
-
-  final String base;
-  final String quote;
-  final String rate;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-      child: Card(
-        color: Colors.lightBlueAccent,
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-          child: Text(
-            '1 $base = $rate $quote',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.white,
-            ),
-          ),
-        ),
       ),
     );
   }
